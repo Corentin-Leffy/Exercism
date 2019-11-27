e653367b5231ac612345ee41f69426cdf910bed0
MrLeffy <corentin.leffy@gmail.com>
2019-11-27 08:25:39 +0100

    Add kotlin solution for allergies problem

---
+++ kotlin/allergies/src/main/kotlin/Allergies.kt
@@ -0,0 +1,8 @@
+class Allergies(private val score: Int) {
+
+    fun isAllergicTo(allergen: Allergen): Boolean =
+        score and allergen.score != 0
+
+    fun getList(): List<Allergen> =
+        Allergen.values().filter { isAllergicTo(it) }
+}

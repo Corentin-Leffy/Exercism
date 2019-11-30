5ac8cb9eb03bf3e04f77364cbbf3be257aaa5ad6
MrLeffy <corentin.leffy@gmail.com>
2019-11-30 11:05:19 +0100

    Add dart solution for pangram problem

---
+++ dart/pangram/lib/pangram.dart
@@ -0,0 +1,33 @@
+class Pangram {
+  Set<String> alphabet = {
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  };
+
+  bool isPangram(String sentence) =>
+      sentence.toLowerCase().split('').toSet().containsAll(alphabet);
+}

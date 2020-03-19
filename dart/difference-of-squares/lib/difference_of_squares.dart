01b24212750d382109a4e7c83592f24ab1648058
Corentin Leffy <corentin.leffy@gmail.com>
2020-03-19 08:20:23 +0100

    Improve Difference of Squares Dart problem

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,12 +1,23 @@
 import 'dart:math';
 
 class DifferenceOfSquares {
-  num squareOfSum(int n) => pow(List.generate(n, (i) => i + 1).reduce(_sum), 2);
+  num squareOfSum(int n) => _from1To(n).sum().square();
 
-  num sumOfSquare(int n) =>
-      List.generate(n, (i) => pow(i + 1, 2).toInt()).reduce(_sum);
+  num sumOfSquare(int n) => _from1To(n).mapToSquares().sum();
 
   num differenceOfSquares(int n) => squareOfSum(n) - sumOfSquare(n);
 
-  int _sum(int left, int right) => left + right;
+  List<num> _from1To(int n) => List.generate(n, (i) => i + 1);
+}
+
+extension on Iterable<num> {
+  num sum() => reduce(_sum);
+
+  num _sum(num previous, num next) => previous + next;
+
+  Iterable<num> mapToSquares() => map((n) => n.square());
+}
+
+extension on num {
+  num square() => pow(this, 2);
 }

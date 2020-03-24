431c65419c5d3edfdfc4c647a40bf6552bd30483
Corentin Leffy <corentin.leffy@gmail.com>
2020-03-24 20:28:40 +0100

    Improve Word count solution in Dart

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,19 +1,18 @@
 class WordCount {
   static final invalidCharacters = RegExp(r"(?!'\w)([\W']+|['\W])");
 
-  Map<String, int> countWords(String input) {
-    String sentence = input.toLowerCase().replaceAll(invalidCharacters, " ");
-    return sentence
-        .split(" ")
-        .where((word) => word.isNotEmpty)
-        .fold({}, (map, word) => _sum(map, {word: _count(word, sentence)}));
-  }
+  Map<String, int> countWords(String input) => input
+      .split(invalidCharacters)
+      .where((word) => word.isNotEmpty)
+      .map((word) => word.toLowerCase())
+      .groupBy((word) => word)
+      .map((word, values) => MapEntry(word, values.length));
+}
 
-  int _count(String word, String input) =>
-      RegExp("\\b($word)\\b").allMatches(input).length;
-
-  Map<String, int> _sum(Map<String, int> map, Map<String, int> word) {
-    map.addAll(word);
+extension GroupBy<S> on Iterable<S> {
+  Map<T, List<S>> groupBy<T>(T Function(S) key) {
+    final Map<T, List<S>> map = {};
+    forEach((element) => (map[key(element)] ??= []).add(element));
     return map;
   }
 }

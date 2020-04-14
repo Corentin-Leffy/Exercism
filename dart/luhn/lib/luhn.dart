a2942abd13ea6ff0d0cca3325fc7b28978c1828b
Corentin Leffy <corentin.leffy@gmail.com>
2020-04-14 07:31:51 +0200

    Improve Luhn problem in Dart

--- dart/luhn/lib/luhn.dart
+++ dart/luhn/lib/luhn.dart
@@ -1,33 +1,34 @@
 class Luhn {
   bool valid(String input) =>
-      _containsOnlyDigits(input) &&
-      _hasAtLeast2Digits(input) &&
-      _reverseToIndexedMap(_sanitize(input))
-                  .map((entry) => _doubleSecondDigit(entry))
-                  .map((value) => _substractDigitGreaterThen9(value))
+      input.containsOnlyDigits &&
+      input.sanitize.hasAtLeast2Digits &&
+      input.sanitize
+                  .toReversedIndexedMap()
+                  .map(_doubleEvenDigit)
+                  .map(_subtractDigitGreaterThen9)
                   .sum() % 10 == 0;
 
-  bool _hasAtLeast2Digits(String input) => _sanitize(input).length > 1;
+  int _doubleEvenDigit(MapEntry<int, int> entry) =>
+      entry.key.isEven ? entry.value : entry.value * 2;
 
-  String _sanitize(String input) => input.replaceAll(' ', '');
-
-  bool _containsOnlyDigits(String input) => !input.contains(RegExp(r'[^0-9 ]'));
-
-  List<MapEntry<int, int>> _reverseToIndexedMap(String input) =>
-      input.toInts().reversed.toIndexedMap();
-
-  int _doubleSecondDigit(MapEntry<int, int> entry) =>
-      entry.key % 2 == 0 ? entry.value : entry.value * 2;
-
-  int _substractDigitGreaterThen9(int value) => value > 9 ? value - 9 : value;
+  int _subtractDigitGreaterThen9(int digit) => digit > 9 ? digit - 9 : digit;
 }
 
 extension on String {
-  List<int> toInts() => split('').map((char) => int.parse(char)).toList();
+  bool get containsOnlyDigits => !contains(RegExp(r'[^0-9 ]'));
+
+  String get sanitize => replaceAll(' ', '');
+
+  bool get hasAtLeast2Digits => length > 1;
+
+  List<MapEntry<int, int>> toReversedIndexedMap() =>
+      _toDigits().reversed.toIndexedMap();
+
+  List<int> _toDigits() => split('').map((char) => int.parse(char)).toList();
 }
 
 extension on Iterable<int> {
   List<MapEntry<int, int>> toIndexedMap() => toList().asMap().entries.toList();
 
-  int sum() => fold<int>(0, (prev, element) => prev + element);
+  int sum() => fold(0, (prev, element) => prev + element);
 }

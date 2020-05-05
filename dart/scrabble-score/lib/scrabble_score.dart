82f60edc68e2e21c5a985fae2e288f354dfb5a9e
Corentin Leffy <corentin.leffy@gmail.com>
2020-05-05 08:09:32 +0200

    Improve Scrabble score solution with runes in Dart

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,18 +1,18 @@
-const scrabble = {
-  ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]: 1,
-  ["D", "G"]: 2,
-  ["B", "C", "M", "P"]: 3,
-  ["F", "H", "V", "W", "Y"]: 4,
-  ["K"]: 5,
-  ["J", "X"]: 8,
-  ["Q", "Z"]: 10
+const _scrabble = {
+  [65, 69, 73, 79, 85, 76, 78, 82, 83, 84]: 1,
+  [68, 71]: 2,
+  [66, 67, 77, 80]: 3,
+  [70, 72, 86, 87, 89]: 4,
+  [75]: 5,
+  [74, 88]: 8,
+  [81, 90]: 10
 };
 
 num score(String word) => word
     .toUpperCase()
-    .split('')
+    .runes
     .map(valueForLetter)
-    .fold(0, (total, value) => total += value);
+    .fold(0, (total, value) => total + value);
 
-num valueForLetter(String letter) =>
-    scrabble.entries.firstWhere((entry) => entry.key.contains(letter)).value;
+num valueForLetter(int letter) =>
+    _scrabble.entries.firstWhere((entry) => entry.key.contains(letter)).value;

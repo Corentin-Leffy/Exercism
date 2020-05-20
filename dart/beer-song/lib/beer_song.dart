af1d37beb073c3de2a9ee610298336369c6a538b
Corentin Leffy <corentin.leffy@gmail.com>
2020-05-20 23:54:13 +0200

    Improve Beer song solution in Dart

--- dart/beer-song/lib/beer_song.dart
+++ dart/beer-song/lib/beer_song.dart
@@ -1,38 +1,36 @@
 class BeerSong {
-  List<String> recite(int startBottles, int bottles) {
-    List<String> song = List.generate(bottles, (i) => startBottles - 1 * i)
-        .map((numberOfBottle) => getSentenceFor(numberOfBottle) + [''])
-        .fold([], (song, sentence) => song + sentence)
-          ..removeLast();
-    return song;
-  }
+  List<String> recite(int startBottles, int bottles) =>
+      List.generate(bottles, (i) => startBottles - 1 * i)
+          .map(_sentences)
+          .fold([], (song, sentence) => song + sentence)
+            ..removeLast();
 
-  List<String> getSentenceFor(int bottles) {
+  List<String> _sentences(int bottles) {
     switch (bottles) {
       case 0:
         return [
           "No more bottles of beer on the wall, no more bottles of beer.",
-          "Go to the store and buy some more, 99 bottles of beer on the wall."
+          "Go to the store and buy some more, 99 bottles of beer on the wall.",
+          ""
         ];
-        break;
       case 1:
         return [
           "1 bottle of beer on the wall, 1 bottle of beer.",
-          "Take it down and pass it around, no more bottles of beer on the wall."
+          "Take it down and pass it around, no more bottles of beer on the wall.",
+          ""
         ];
-        break;
       case 2:
         return [
           "2 bottles of beer on the wall, 2 bottles of beer.",
-          "Take one down and pass it around, 1 bottle of beer on the wall."
+          "Take one down and pass it around, 1 bottle of beer on the wall.",
+          ""
         ];
-        break;
       default:
         return [
           "$bottles bottles of beer on the wall, $bottles bottles of beer.",
-          "Take one down and pass it around, ${bottles - 1} bottles of beer on the wall."
+          "Take one down and pass it around, ${bottles - 1} bottles of beer on the wall.",
+          ""
         ];
-        break;
     }
   }
 }
